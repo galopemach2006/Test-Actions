90f8a1c493ce20bbcb62f764a592251190f43f8d
galopemach@gmail.com <galopemach@gmail.com>
2026-06-14 23:22:02 +0800

    Fixed Add Workflow Dispatch

--- Playwright/tests/login.spec.js
+++ Playwright/tests/login.spec.js
@@ -3,5 +3,5 @@ const { test, expect } = require('@playwright/test');
 test('Youtube title test', async ({ page }) => {
     await page.goto('https://www.youtube.com');
 
-    await expect(page).toHaveTitle(/Messenger/);
+    await expect(page).toHaveTitle(/YouTube/);
 });

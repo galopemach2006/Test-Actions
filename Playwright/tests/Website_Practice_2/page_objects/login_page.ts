ac0cf4542b9f4ec67734c74ae39c8a8d23f693d4
galopemach@gmail.com <galopemach@gmail.com>
2026-06-20 16:45:21 +0800

    finished login_testing_website_practice_2

--- Playwright/tests/Website_Practice_2/page_objects/login_page.ts
+++ Playwright/tests/Website_Practice_2/page_objects/login_page.ts
@@ -1,45 +1,72 @@
-import { Page, expect } from "@playwright/test";
+import { Page, expect, Locator } from "@playwright/test";
 
 export class LoginPage {
     readonly page: Page;
+    readonly usernameInput: Locator;
+    readonly passwordInput: Locator;
+    readonly successHeader: Locator;
+    readonly button: Locator;
+    readonly bothCredentialsError: Locator;
+    readonly wrongEmailError: Locator;
+    readonly wrongPasswordError: Locator;
+    readonly requiredUsername: Locator;
+    readonly requiredPassword: Locator;
 
     constructor(page : Page) {
         this.page = page;
+        this.usernameInput = page.getByPlaceholder("eg. user@user.com")
+        this.passwordInput = page.getByPlaceholder("*******")
+        this.successHeader = page.getByRole('heading', {name: "Login Successful"})
+        this.button = page.getByRole('button', {name: "Login"})
+        this.bothCredentialsError = page.getByText("Your email and password both are invalid!")
+        this.wrongEmailError = page.getByText("Your email is invalid!")
+        this.wrongPasswordError = page.getByText("Your password is invalid!")
+        this.requiredUsername = page.getByText("Email is a required field")
+        this.requiredPassword = page.getByText("Password is a required field")
     }
 
     async navigate() {
         await this.page.goto("https://practice.qabrains.com/")
     }
 
-    async Username_Password_Verifier(username : string, password : string) {
-        const user = await this.page.getByPlaceholder("eg. user@user.com")
-        const pass = await this.page.getByPlaceholder("*******")
-
-        await user.fill(username)
-        await pass.fill(password)
-
-        await this.page.getByRole('button', {name: "Login"}).click()
-        const header = this.page.getByRole('heading', {name: "Login Successful"})
-        const both_invalid = this.page.getByText("Your email and password both are invalid!")
-        const no_password = this.page.getByText("Password is a required field")
-        const no_email = this.page.getByText("Email is a required field")
+    async username_password_verifier(username : string, password : string) {
+        await this.usernameInput.fill(username)
+        await this.passwordInput.fill(password)
+        await this.button.click()
 
         if (username == "qa_testers@qabrains.com" && password == "Password123") {
             console.log("Right Credentials")
-            await expect(header).toBeVisible({timeout: 5000})
-        } else if(username == "qa_testers@qabrains.com" && password == ""){
-            console.log("Missing Password")
-            await expect(no_password).toBeVisible()
-        } else if(username == "" && password == "Password123") {
-            console.log("Missing Email")
-            await expect(no_email).toBeVisible()
-        } else {
+            await expect(this.successHeader).toBeVisible({timeout: 5000})
+        } else if(username == "qa_testers@qabrains" && password == "Password123") {
+            console.log("Wrong Email, Right Password")
+            await expect(this.wrongEmailError).toBeVisible
+            await expect(this.successHeader).toBeHidden() 
+        } else if(username == "qa_testers@qabrains.com" && password == "Password") {
+            console.log("Right Email, Wrong Password")
+            await expect(this.wrongPasswordError).toBeVisible()
+            await expect(this.successHeader).toBeHidden() 
+        }  else {
             console.log("Both Wrong Credentials")
-            await expect(header).toBeHidden()
-            await expect(both_invalid).toBeVisible()
+            await expect(this.successHeader).toBeHidden()
+            await expect(this.bothCredentialsError).toBeVisible()
         }
+        await this.page.waitForTimeout(1000)
+    }
 
-        await this.page.waitForTimeout(2000)
+    async username_password_blank(username : string, password: string) {
+       await this.usernameInput.fill(username)
+       await this.passwordInput.fill(password)
+       await this.button.click()
+
+       if (username == "" && password == "Password123") {
+            console.log("Missing Email")
+            await expect(this.requiredUsername).toBeVisible()
+            await expect(this.successHeader).toBeHidden()
+       } else if (username == "qa_testers@qabrains.com" && password == "") {
+            console.log("Missing Password")
+            await expect(this.requiredPassword).toBeVisible()
+            await expect(this.successHeader).toBeHidden()
+       }
     }
 
 }
--- Playwright/tests/Website_Practice_2/tests/login_testing.spec.ts
+++ Playwright/tests/Website_Practice_2/tests/login_testing.spec.ts
@@ -6,20 +6,27 @@ test("Login By POM", async ({page}) => {
 
     //Right Credentials
     await login.navigate()
-    await login.Username_Password_Verifier("qa_testers@qabrains.com", "Password123")
+    await login.username_password_verifier("qa_testers@qabrains.com", "Password123")
 
     //Both Wrong
     await login.navigate()
-    await login.Username_Password_Verifier("qa_testers@qbrains.com", "Passd123")
+    await login.username_password_verifier("qa_testers@qbrains.com", "Passd123")
+
+    //Wrong Email, Right Password
+    await login.navigate()
+    await login.username_password_verifier("qa_testers@qabrains.com", "Password123")
+
+    //Right Email, Wrong Password
+    await login.navigate()
+    await login.username_password_verifier("qa_testers@qabrains.com", "Password")
 
     //Missing Password
     await login.navigate()
-    await login.Username_Password_Verifier("qa_testers@qabrains.com", "")
-
-    //Missing Username
+    await login.username_password_blank("qa_testers@qabrains.com", "")
+    
+    //Missing Email
     await login.navigate()
-    await login.Username_Password_Verifier("", "Password123")
-
-    //Right Password, Wrong Email
-    //Right Email, Wrong Password
+    await login.username_password_blank("", "Password123")
+    
+    
 })

297c190eaccf146657bbf5bcfbc601ffd84aeb9b
galopemach@gmail.com <galopemach@gmail.com>
2026-06-22 19:41:54 +0800

    did some changes again

--- Playwright/playwright.config.ts
+++ Playwright/playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    video: 'on',
   },
 
   /* Configure projects for major browsers */
--- Playwright/tests/QA_Brains/tests/form_submission_testing.spec.ts
+++ Playwright/tests/QA_Brains/tests/form_submission_testing.spec.ts
@@ -1,19 +1,23 @@
 import { test, expect } from "@playwright/test";
 import { forms } from "../page-objects/form_submission";
-import forms_data from "../test-data/forms_data.json"
+import forms_data from "../test-data/forms_data.json";
 
 test.describe("Submission Forms Testing", () => {
-  let f: forms  
-  const data = forms_data.allData
+  let f: forms;
+  const data = forms_data.allData;
 
-  test.beforeEach(async ({page}) => {
-    f = new forms(page)
+  test.beforeEach(async ({ page }) => {
+    f = new forms(page);
     await f.navigate();
+  });
+
+  test.afterAll(async() => {
+    console.log("Testing Complete")
   })
 
   test("Complete Information", async () => {
-    await f.fillInformation(data.completeData)
-    await expect(f.success).toBeVisible()
+    await f.fillInformation(data.completeData);
+    await expect(f.success).toBeVisible();
   });
 
   test("Blank Information", async () => {
@@ -22,20 +26,29 @@ test.describe("Submission Forms Testing", () => {
   });
 
   test("Email Verification - Invalid Format", async () => {
-    const newData = {...data.completeData, email: data.invalidData.invalidEmail1}
-    await f.fillInformation(newData)
+    const newData = { ...data.completeData, email: data.invalidData.invalidEmail1 };
+    await f.fillInformation(newData);
     await f.emailInvalidFormat(data.expectedMessage.jsMessage1);
   });
 
   test("Email Verification - Blank Email", async () => {
-    const newData = {...data.completeData, email: data.blankData.email}
-    await f.fillInformation(newData)
-    await expect(f.page.getByText("Email is a required field")).toBeVisible()
-  })
+    const newData = { ...data.completeData, email: data.blankData.email };
+    await f.fillInformation(newData);
+    await expect(f.page.getByText("Email is a required field")).toBeVisible();
+  });
 
   test("Email Verification - Invalid Email", async () => {
-    const newData = {...data.completeData, email: data.invalidData.invalidEmail2}
+    const newData = { ...data.completeData, email: data.invalidData.invalidEmail2 };
     await f.fillInformation(newData);
     await f.emailInvalidFormat(data.expectedMessage.jsMessage2);
+  });
+
+  test.skip("All Countries", async ({page}) => {
+    await page.locator("select[name='country']").selectOption("Andorra");
+    let allElements = await page.locator("select[name='country'] option").all();
+
+    for (const e of allElements) {
+      console.log(e + "\n");
+    }
   });
 });
